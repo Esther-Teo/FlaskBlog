bdc4a46f4887515aebc36a0ac13c83c0e40a6db3
Esther-Teo <esther.teo.min@gmail.com>
2020-07-23 03:34:37 +0800

    Flask Blueprinting(In Progress)

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,30 +1,29 @@
 #Initialise application and bring tgt various components
-import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
 #importing flask class
 app = Flask(__name__)
+app.config.from_object(Config)
 #__name__ = special variable that is name of module
 #below pretend database call
-app.config['SECRET_KEY'] = '73a2793b82d0e48b9bedec419b62112a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-#These are environmental variables stored in the OS
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
-from flaskblog import routes
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
