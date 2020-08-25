f217928979f845912b12a5b409da93af274576ee
Esther-Teo <esther.teo.min@gmail.com>
2020-08-25 21:14:31 +0800

    Blueprints

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -15,9 +15,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-
 mail = Mail(app)
 
 
---
+++ ~/.bash_profile
@@ -0,0 +1,4 @@
+
+export SECRET_KEY='73a2793b82d0e48b9bedec419b62112a'
+export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+
