f2a890abcad801d6f5b98a905350d02b55f2c588
Esther Teo <esther.teo.min@gmail.com>
2025-11-09 13:37:27 +0800

    FEATURE: Set up file for initialization of db

---
+++ create_db.py
@@ -0,0 +1,7 @@
+from flaskblog import create_app, db
+from flaskblog.models import User, Post
+
+app = create_app()
+with app.app_context():
+    db.create_all()
+    print("Database tables created successfully.")
